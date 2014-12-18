60bd411e4257204b12342e8713782232d7e1f469
David <davidfallah1@gmail.com>
2014-12-18 00:33:46 +0000

    Add version constructor

--- src/main/groovy/tagc/semver/Version.groovy
+++ src/main/groovy/tagc/semver/Version.groovy
@@ -18,6 +18,13 @@ class Version {
     final int patch = 0
     final boolean release = false
 
+    Version(int major = 0, int minor = 0, int patch = 0, boolean release = false) {
+        this.major = major;
+        this.minor = minor;
+        this.patch = patch;
+        this.release = release;
+    }
+
     @Override
     String toString() {
         "$major.$minor.$patch{release? '': '-SNAPSHOT'}"
