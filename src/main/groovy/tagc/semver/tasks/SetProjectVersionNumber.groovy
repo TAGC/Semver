7480fa078ad3453b2b311c3411472d1e2fef3d89
David <davidfallah1@gmail.com>
2014-12-25 22:24:17 +0000

    Add additional tests for SetProjectVersionNumber task

--- src/main/groovy/tagc/semver/tasks/SetProjectVersionNumber.groovy
+++ src/main/groovy/tagc/semver/tasks/SetProjectVersionNumber.groovy
@@ -1,5 +1,8 @@
 package tagc.semver.tasks
 
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
 import org.ajoberstar.grgit.Grgit
 import org.eclipse.jgit.errors.RepositoryNotFoundException
 import org.gradle.api.DefaultTask
@@ -10,58 +13,59 @@ import org.gradle.api.tasks.TaskAction
 import tagc.semver.Version
 
 class SetProjectVersionNumber extends DefaultTask {
-    
+
     private static final String MASTER_BRANCH = "master"
-    
+    private static final Pattern VERSION_PATTERN = ~/(\d+)\.(\d+).(\d+)/
+
     @Input String versionFilePath
     private Grgit repo
-    
+
     SetProjectVersionNumber() {
         this.group = 'semver'
         this.description = "Sets the semantic version number of the project."
     }
-    
+
     @TaskAction
-    void setProjectVersionNumber() {
+    void start() {
         try {
-            this.repo = Grgit.open(project.file('.'))
+            this.repo = Grgit.open(project.file("$project.projectDir"))
         } catch(RepositoryNotFoundException e) {
             throw new GradleException("No Git repository can be found for this project")
         }
-        
+
         def rawVersion = readRawVersion()
-        
+
         if (isOnMasterBranch()) {
-            project.version = rawVersion.toRelease().toString()   
+            project.version = rawVersion.toRelease().toString()
         } else {
             project.version = rawVersion.toDevelop().toString()
         }
     }
-    
+
     private Version readRawVersion() {
         def versionFile = new File(getVersionFilePath())
         if (!versionFile.exists()) {
             throw new GradleException("Missing version file: $versionFile.canonicalPath")
         }
-        
-        final Properties versionProps = new Properties()
-        
-        versionFile.withInputStream { stream ->
-            versionProps.load(stream)
+
+        Matcher m = versionFile.text =~ VERSION_PATTERN
+
+        if(!m) {
+            throw new GradleException("Cannot extract version information from specified version file")
         }
-        
+
         def builder = new Version.Builder()
-        builder.setMajor(versionProps.major.toInteger())
-        builder.setMinor(versionProps.minor.toInteger())
-        builder.setPatch(versionProps.patch.toInteger())
+        builder.setMajor(m[0][1].toInteger())
+        builder.setMinor(m[0][2].toInteger())
+        builder.setPatch(m[0][3].toInteger())
         builder.getVersion()
     }
-    
+
     private boolean isOnMasterBranch() {
         getCurrentBranch() == MASTER_BRANCH
     }
 
     private String getCurrentBranch() {
         repo.branch.current
-    }   
+    }
 }
--- src/test/groovy/tagc/semver/tasks/SetProjectVersionNumberSpec.groovy
+++ src/test/groovy/tagc/semver/tasks/SetProjectVersionNumberSpec.groovy
@@ -1,5 +1,7 @@
 package tagc.semver.tasks
 
+import org.ajoberstar.grgit.Grgit
+import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.testfixtures.ProjectBuilder
@@ -28,5 +30,71 @@ class SetProjectVersionNumberSpec extends Specification {
         task != null
         task.group == 'semver'
         task.versionFilePath == 'version.properties'
+    }
+    
+    def "Referencing non-existent file throws exception"() {
+        final String nonExistentFilePath = '.nonexistent'
+        
+        expect:
+        !new File(nonExistentFilePath).exists()
+        project.tasks.findByName(TASK_NAME) == null
+        
+        when:
+        final Task task = project.task(TASK_NAME, type: SetProjectVersionNumber) {
+            versionFilePath = nonExistentFilePath
+        }
+        
+        task.start()
+        
+        then:
+        project.tasks.findByName(TASK_NAME) != null
+        Exception e = thrown(GradleException)
+        e.toString() =~ 'Git'
+    }
+    
+    def "Referencing existent file with incorrect format throws exception"() {
+        final String filePath = 'invalidTestVersion.properties'
+        final Grgit grgit = Grgit.init(dir: new File("$project.projectDir"))
+        
+        expect:
+        new File(filePath).exists()
+        
+        when:
+        final Task task = project.task(TASK_NAME, type: SetProjectVersionNumber) {
+            versionFilePath = filePath
+        }
+        
+        task.start()
+        
+        then:
+        project.tasks.findByName(TASK_NAME) != null
+        Exception e = thrown(GradleException)
+        e.toString() =~ 'extract'
+        
+        cleanup:
+        grgit.close()
+    }
+    
+    def "Referencing a valid version file should succeed"() {
+        final String filePath = 'validTestVersion.properties'
+        final Grgit grgit = Grgit.init(dir: new File("$project.projectDir"))
+        
+        expect:
+        new File(filePath).exists()
+        new File(filePath).text =~ /version='1\.2\.3'/
+        
+        when:
+        final Task task = project.task(TASK_NAME, type: SetProjectVersionNumber) {
+            versionFilePath = filePath
+        }
+        
+        task.start()
+        
+        then:
+        project.tasks.findByName(TASK_NAME) != null
+        project.version ==~ /1\.2\.3.*/
+        
+        cleanup:
+        grgit.close()
     }
 }
